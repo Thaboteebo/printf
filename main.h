a35fc1dcd5cef6d691785bf60bf072dfc93bcdf1
Mbccode <bmkpume@yahoo.com>
2023-08-20 00:03:18 -0700

    Main header file

---
+++ main.h
@@ -0,0 +1,15 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdarg.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+int _putchar(char c);
+
+/****Task 0 FUNCTION****/
+int _printf(const char *format, ...);
+
+
+#endif
